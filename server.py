2e7d30ccf3c1aded375bb010a595b18ca850ab85
prathijna14 <prathijnah@gmail.com>
2023-04-12 18:29:58 +0530

    endpoient for monthly_performance

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
-
 from fastapi import FastAPI
 from schema import student_performance, Performance, PerformanceBySubject
+import pandas as pd
+import pyodbc
+from datetime import datetime
+from pydantic import BaseModel
+
+
 
 app = FastAPI()
 
@@ -19,137 +24,7 @@ def evaluate(student: student_performance):
         }
 
 
-    
-"""
-@app.post("/api/v1/getPerformanceBySubject", response_model=Performance)
-def getAvgPerformanceByStudentWeekly(student: student_performance):
-    performance = (student.correct_answer / student.total_quetion) * 100
-    percentage = (student.attempted_quetion/ student.total_quetion) * 100
-    accuracy = (student.correct_answer / student.attempted_quetion) * 100
-    efficiency = (student.attempted_quetion*60 / student.time_taken)
-    return {
-            "performance": performance,
-            "percentage": percentage,
-            "accuracy": accuracy,
-            "efficiency": efficiency,
-        }
 
-
-@app.post("/api/v1/getPerformanceBySubject/{subject_id}/{student_id}", response_model=Performance)
-def getPerformanceBySubject(student: PerformanceBySubject):
-    performance = (student.correct_answer / student.total_quetion) * 100
-    percentage = (student.attempted_quetion/ student.total_quetion) * 100
-    accuracy = (student.correct_answer / student.attempted_quetion) * 100
-    efficiency = (student.time_taken / student.time_given)
-    return {
-            "performance": performance,
-            "percentage": percentage,
-            "accuracy": accuracy,
-            "efficiency": efficiency,
-        }
-
-
-@app.post("/api/v1/getAvgPerformanceByStudentWeekly", response_model=Performance)
-def getAvgPerformanceByStudentWeekly(student: student_performance):
-    performance = (student.correct_answer / student.total_quetion) * 100
-    percentage = (student.attempted_quetion/ student.total_quetion) * 100
-    accuracy = (student.correct_answer / student.attempted_quetion) * 100
-    efficiency = (student.time_taken / student.time_given)
-    return {
-            "performance": performance,
-            "percentage": percentage,
-            "accuracy": accuracy,
-            "efficiency": efficiency,
-        }
-
-
-@app.post("/api/v1/getAvgPerformanceByStudentMonthly", response_model=Performance)
-def getAvgPerformanceByStudentMonthly(student: student_performance):
-    performance = (student.correct_answer / student.total_quetion) * 100
-    percentage = (student.attempted_quetion/ student.total_quetion) * 100
-    accuracy = (student.correct_answer / student.attempted_quetion) * 100
-    efficiency = (student.time_taken / student.time_given)
-    return {
-            "performance": performance,
-            "percentage": percentage,
-            "accuracy": accuracy,
-            "efficiency": efficiency,
-        }
-
-from typing import List
-from fastapi import FastAPI
-from pydantic import BaseModel
-import pyodbc
-import datetime
-
-app = FastAPI()
-
-# Function to get the week number for a given date
-def get_week_number_by_date(date_str: str) -> int:
-    date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
-    return date_obj.isocalendar()[1]
-
-# Model for the request payload
-class StudentWeeklyRequest(BaseModel):
-    student_id: int
-    week_number: int
-    date: str
-
-# Model for the response payload
-class StudentWeeklyResponse(BaseModel):
-    accuracy: float
-    correct_percentage: float
-    efficiency: float
-    student_id: int
-    test_id: int
-    total_score: float
-
-# Function to calculate the average performance by student weekly
-def calculate_average_performance_by_student_weekly(student_id: int, week_number: int) -> List[StudentWeeklyResponse]:
-    # Connect to the database
-    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=LAPTOP-DBB6O6KQ;DATABASE=performance_db;UID=prathi;PWD=p@123')
-
-    cursor = conn.cursor()
-
-    # Execute the SQL query to get the data for the given student and week number
-    query = f"SELECT accuracy, correct_percentage, efficiency, student_id, test_id, total_score FROM result_analysis WHERE student_id = {student_id} and WEEK(date_taken) = {week_number}"
-    cursor.execute(query, (student_id, week_number))
-
-    # Fetch the results and calculate the averages
-    results = cursor.fetchall()
-    num_results = len(results)
-    accuracy_sum = sum([result[0] for result in results])
-    correct_percentage_sum = sum([result[1] for result in results])
-    efficiency_sum = sum([result[2] for result in results])
-    total_score_sum = sum([result[5] for result in results])
-    averages = {
-        "accuracy": accuracy_sum / num_results,
-        "correct_percentage": correct_percentage_sum / num_results,
-        "efficiency": efficiency_sum / num_results,
-        "student_id": student_id,
-        "test_id": results[0][4],
-        "total_score": total_score_sum / num_results
-    }
-
-    # Close the database connection and return the averages
-    conn.close()
-    return [StudentWeeklyResponse(**averages)]
-
-# Define the API endpoint for the getAvgPerformanceByStudentWeekly API
-@app.post("/getAvgPerformanceByStudentWeekly")
-async def get_avg_performance_by_student_weekly(request: StudentWeeklyRequest):
-    week_number = request.week_number
-    if week_number == -1:
-        week_number = get_week_number_by_date(request.date)
-    return calculate_average_performance_by_student_weekly(request.student_id, week_number)
-
-
-"""
-import pandas as pd
-import pyodbc
-from fastapi import FastAPI
-
-app = FastAPI()
 
 # Database configuration details
 driver = "{ODBC Driver 17 for SQL Server}"
@@ -192,4 +67,107 @@ def calculate_ewma(student_id: int, subject_id: int):
 
 
 
+
+
+
+
+# Create connection to SQL Server
+driver = "{ODBC Driver 17 for SQL Server}"
+server = "LAPTOP-DBB6O6KQ"
+database = "performance_db"
+username = "prathi"
+password = "p@123"
+
+conn = pyodbc.connect(f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}")
+
+# Define model for incoming data
+class ResultAnalysis(BaseModel):
+    accuracy: int
+    correct_percentage: int
+    efficiency: int
+    student_id: int
+    subject_id: int
+    test_id: int
+    total_score: int
+    subject_id: int
+    test_date: str
+
+# Define route to calculate monthly performance
+@app.post("/calculate_monthly_performance")
+async def calculate_monthly_performance():
+    # Get current month and year
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+
+    # Fetch data from result_analysis table
+    cursor = conn.cursor()
+    cursor.execute("SELECT accuracy, correct_percentage, efficiency, student_id, test_id, total_score, subject_id, test_date FROM result_analysis")
+    rows = cursor.fetchall()
+
+    # Iterate through the rows and calculate monthly performance
+    for row in rows:
+        accuracy = row.accuracy
+        correct_percentage = row.correct_percentage
+        efficiency = row.efficiency
+        student_id = row.student_id
+        test_id = row.test_id
+        total_score = row.total_score
+        subject_id = row.subject_id
+        test_date = row.test_date
+
+         # Convert datetime.date object to string
+        test_date_str = test_date.strftime("%Y-%m-%d")
+
+        # Use the string version of test_date in strptime()
+        test_date = datetime.strptime(test_date_str, "%Y-%m-%d")
+
+
+        # Extract month and year from test_date
+        month = test_date.month
+        year = test_date.year
+
+        # Check if the test_date is in the current month and year
+        if month == current_month and year == current_year:
+            # Insert calculated monthly performance into monthly_performance table
+            cursor.execute("INSERT INTO monthly_performance (student_id, subject_id, month, year, accuracy, correct_percentage, efficiency, total_score) VALUES (?, ?, ?, ?, ?, ?,?,?)",
+                           student_id, subject_id, month, year, accuracy, correct_percentage, efficiency, total_score)
+            cursor.commit()
+
+    return {"message": "Monthly performance calculated and stored successfully!"}
+
+
+# Define model for incoming data
+class MonthlyPerformance(BaseModel):
+    student_id: int
+    subject_id: int
+    month:int
+
+# Define route to get monthly performance
+@app.post("/get_monthly_performance")
+async def get_monthly_performance(data: MonthlyPerformance):
+    student_id = data.student_id
+    subject_id = data.subject_id
+    month = data.month
+
+    # Fetch data from monthly_performance table based on student_id, subject_id, and month
+    cursor = conn.cursor()
+    cursor.execute("SELECT student_id, subject_id, month, year, accuracy, correct_percentage, efficiency, total_score FROM monthly_performance WHERE student_id = ? AND subject_id = ? AND month = ?",
+                   (student_id, subject_id, month)) # corrected: added missing 'AND' in SQL query and passing parameters as tuple
+    rows = cursor.fetchall()
+
+    # Format retrieved data as a list of dictionaries
+    monthly_performance = []
+    for row in rows:
+        monthly_performance.append({
+            "student_id": row.student_id,
+            "subject_id": row.subject_id,  # corrected: changed 'row.subject' to 'row.subject_id'
+            "month": row.month,
+            "year": row.year,
+            "accuracy": row.accuracy,
+            "correct_percentage": row.correct_percentage,
+            "efficiency": row.efficiency,
+            "total_score": row.total_score
+        })
+
+    return {"monthly_performance": monthly_performance}
 
